55517badc9f98a9a07bad21a7e9ca4c2385a0cd3
Akshat Shah <akshat.shah@student.allenisd.org>
2020-04-13 15:09:58 -0500

    init commit

--- Sources/ScenesShell/Background.swift
+++ Sources/ScenesShell/Background.swift
@@ -1,15 +1,27 @@
 import Scenes
 import Igis
 
-  /*
-     This class is responsible for rendering the background.
-   */
+/*
+ This class is responsible for rendering the background.
+ */
 
 
 class Background : RenderableEntity {
+    let text : Text
+    let rectangle : Rectangle
+    
+    init() {
+        text = Text(location:Point(x:50, y:50), text:"Hello, World!")
+        text.font = "30pt Arial"
 
-      init() {
-          // Using a meaningful name can be helpful for debugging
-          super.init(name:"Background")
-      }
+        let rect = Rect(topLeft:Point(x:20, y:10), size:Size(width:300, height:50))
+        rectangle = Rectangle(rect:rect, fillMode:.stroke)
+        
+        // Using a meaningful name can be helpful for debugging
+        super.init(name:"Background")
+    }
+
+    override func setup(canvasSize:Size, canvas:Canvas) {
+        canvas.render(text, rectangle)
+    }
 }
